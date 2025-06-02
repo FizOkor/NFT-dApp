239e6c8f472e7440e760cc77d6f310a64c909664
FizOkor <okorfavour2000@gmail.com>
2025-06-02 11:15:43 +0100

    uploadtoNftStorage adjustment

--- nft_daap/src/assets/uploadToNFTStorage.js
+++ nft_daap/src/assets/uploadToNFTStorage.js
@@ -4,11 +4,14 @@ export async function uploadToNFTStorage(name, description, imageFile) {
   formData.append('name', name);
   formData.append('description', description);
   
-  console.log(window.location.origin)
-  const response = await fetch(`${window.location.origin}/api/upload-nft`, {
-    method: 'POST',
-    body: formData
-  });
+  try {
+    const response = await fetch(`${window.location.origin}/api/upload-nft`, {
+      method: 'POST',
+      body: formData
+    });
+  } catch (err){
+    console.log(err);
+  }
 
   const { metadataUrl } = await response.json();
   
